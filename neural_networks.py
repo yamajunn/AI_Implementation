30af44ea8735dcbdeb8348253d849afb532d22f4
yamajunn <amiton1115@gmail.com>
2024-10-10 17:38:58 +0900

    Refactor forward propagation and add backward propagation

--- neural_networks.py
+++ neural_networks.py
@@ -35,28 +35,69 @@ def initialize_weights(layer_sizes):  # initialize weights and biases
         biases.append(b)
     return weights, biases
 
-
-def forward_propagation(inputs, weights, biases):
+def forward_propagation(inputs, weights, biases):  # forward propagation
     activations = [inputs]
     for W, b in zip(weights, biases):
-        z = [sum([activations[-1][i] * W[j][i] for i in range(len(activations[-1]))]) + b[j] for j in range(len(b))]
+        z = [
+            sum([activations[-1][i] * W[j][i] for i in range(len(activations[-1]))]) + b[j]
+            for j in range(len(b))
+        ]
         activations.append([relu(z_i) for z_i in z])
     return activations
+
+def backward_propagation(activations, y_true, weights, biases, learning_rate):  # backward propagation
+    output_layer = activations[-1]
+    errors = [
+        (output_layer[i] - y_true[i]) * sigmoid_derivative(output_layer[i])
+        for i in range(len(y_true))
+    ]
+    deltas = [errors]
+    # Backpropagating the hidden layer error
+    for l in range(len(weights)-1, 0, -1):
+        hidden_errors = [
+            sum([deltas[0][k] * weights[l][k][j] for k in range(len(deltas[0]))]) * relu_derivative(activations[l][j])
+            for j in range(len(activations[l]))
+        ]
+        deltas.insert(0, hidden_errors)
+    # Update the weights and biases
+    for l in range(len(weights)):
+        for i in range(len(weights[l])):
+            for j in range(len(weights[l][i])):
+                weights[l][i][j] -= learning_rate * deltas[l][i] * activations[l][j]
+            biases[l][i] -= learning_rate * deltas[l][i]
+
+    return weights, biases
+
+def train(X, y, layer_sizes, epochs, learning_rate):
+    weights, biases = initialize_weights(layer_sizes)
+
+    for epoch in range(epochs):
+        total_loss = 0
+        for i in range(len(X)):
+            activations = forward_propagation(X[i], weights, biases)
+            total_loss += cross_entropy_loss(y[i], activations[-1])
+            weights, biases = backward_propagation(activations, y[i], weights, biases, learning_rate)
+        
+        m = (epoch + (epochs // 20) - 1) // (epochs // 20)
+        print(f"Epoch {epoch+1}/{epochs}, Loss: {total_loss/len(X)}")
+        print(f"[{'+'*m}{' '*(20-m)}]")
+        print("\033[3A")
+    print("\n\nComplete")
+    return weights, biases
+
 
 # DataSet for XOR
 X = [[0, 0], [0, 1], [1, 0], [1, 1]]  # Input
 y = [[0], [1], [1], [0]]  # Output
 
-layer_sizes = [2, 3, 3, 1]  # 2_input -> 3_hidden -> 3_hidden -> 1_output
+epochs = 500  # Number of epochs
+learning_rate = 0.01  # Learning rate
+layer_sizes = [2, 8, 16, 8, 1]  # 2 input -> 8 hidden -> 16 hidden -> 8 hidden -> 1 output
 
-weights, biases = initialize_weights(layer_sizes)
-activations = forward_propagation(X[0], weights, biases)
-print(activations)
-# print(weights)
-# print(biases)
+weights, biases = train(X, y, layer_sizes, epochs, learning_rate)
 
-'''     2   3   3   1  <- layer_sizes
-        #   #   #
-2_input     #   #   #  1_output
-        #   #   #
-'''
+for i in range(len(X)):  # Prediction
+    activations = forward_propagation(X[i], weights, biases)
+    output = activations[-1]
+    binary_output = [1 if o >= 0.5 else 0 for o in output]
+    print(f"Inputs: {X[i]}, Output: {y[i]} Predict: {binary_output}, probability: {round(output[0], 3)}")
