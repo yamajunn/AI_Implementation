a232f2111781411e7bd7d58e320a67781931b683
yamajunn <amiton1115@gmail.com>
2024-10-13 12:38:33 +0900

    Refactor activation functions to use leaky ReLU instead of ReLU

--- titanic.py
+++ titanic.py
@@ -2,14 +2,12 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 # データセットのパスを指定
-train_path = '/Volumes/SSD/titanic/train.csv'
-test_path = '/Volumes/SSD/titanic/test.csv'
-# データセットを読み込む
-train = pd.read_csv(train_path)
-test = pd.read_csv(test_path)
+csv_path = '/Users/chinq500/Desktop/Programs/AI/titanic/train.csv'
 
-train = train.drop(columns=["Name", "PassengerId"])
-test = test.drop(columns=["Name", "PassengerId"])
+# データセットを読み込む
+data = pd.read_csv(csv_path)
+
+data = data.drop(columns=["Name", "PassengerId"])
 # ラベルエンコーダーを作成
 label_encoder = LabelEncoder()
 
@@ -19,15 +17,15 @@ columns_to_encode = ["Sex", "Ticket", "Cabin", "Embarked"]
 
 # 各列をラベルエンコード
 for column in columns_to_encode:
-    train[column] = label_encoder.fit_transform(train[column].astype(str))
-    test[column] = label_encoder.fit_transform(test[column].astype(str))
+    data[column] = label_encoder.fit_transform(data[column].astype(str))
 
 # 欠損値を各列の平均値で補完
-train.fillna(train.mean(), inplace=True)
-test.fillna(test.mean(), inplace=True)
+data.fillna(data.mean(), inplace=True)
 
 from neural_networks import NeuralNetwork
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 # nn = NeuralNetwork(hidden_size=[8, 16, 32, 64, 32, 16, 8], epochs=30, learning_rate=0.01)  # Create a neural network
 # nn.train(train.drop(columns=["Survived"]).values.tolist(), [[y] for y in train["Survived"].values.tolist()])  # Train the neural network
@@ -36,8 +34,9 @@ from sklearn.neural_network import MLPClassifier
 # print(output)
 
 # 特徴量とターゲットを分ける
-X_train = train.drop(columns=["Survived"])
-y_train = train["Survived"]
+X = data.drop(columns=["Survived"])
+y = data["Survived"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # MLPClassifierのインスタンスを作成
 mlp = MLPClassifier(hidden_layer_sizes=(8, 16, 32, 64, 32, 16, 8), max_iter=30, learning_rate_init=0.01)
@@ -46,6 +45,11 @@ mlp = MLPClassifier(hidden_layer_sizes=(8, 16, 32, 64, 32, 16, 8), max_iter=30, 
 mlp.fit(X_train, y_train)
 
 # テストデータで予測
-predictions = mlp.predict(test)
+predictions = mlp.predict(X_test)
 
 print(predictions)
+
+# 精度
+accuracy = accuracy_score(y_test, predictions)
+
+print(f'Accuracy: {accuracy * 100:.2f}%')
